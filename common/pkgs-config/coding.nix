0a5f71ce40cc071b6d0f3c63ce72842682bc5c67
PolarizedIons <polarizedions@gmail.com>
2022-04-19 19:42:25 +0200

    fix issue with nuget package

--- common/pkgs-config/coding.nix
+++ common/pkgs-config/coding.nix
@@ -23,5 +23,11 @@ in {
 
     virtualisation.docker.enable = true;
     users.users.polarizedions.extraGroups = [ "docker" ];
+
+    system.activationScripts.ldso = lib.stringAfter [ "usrbinenv" ] ''
+      mkdir -m 0755 -p /lib64
+      ln -sfn ${pkgs.glibc.out}/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2.tmp
+      mv -f /lib64/ld-linux-x86-64.so.2.tmp /lib64/ld-linux-x86-64.so.2 # atomically replace
+    '';
   };
 }
