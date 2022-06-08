72a85cefbedc97c3af15c5ac416708af0b1046da
PolarizedIons <hi@polarizedions.net>
2022-06-08 21:03:07 +0200

    add polymc

--- common/pkgs-config/games.nix
+++ common/pkgs-config/games.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }:
 with lib; {
   config = mkIf config.setup.coding.enable {
-    environment.systemPackages = with pkgs; [ minecraft ];
+    environment.systemPackages = with pkgs; [ minecraft polymc ];
     programs.steam.enable = true;
   };
 }
