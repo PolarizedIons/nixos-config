46a46fe75592161709fd747d88a05c5b933498dc
PolarizedIons <hi@polarizedions.net>
2022-10-22 13:43:20 +0200

    swap polymc for prismlauncher

--- common/pkgs-config/games.nix
+++ common/pkgs-config/games.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }:
 with lib; {
   config = mkIf config.setup.coding.enable {
-    environment.systemPackages = with pkgs; [ minecraft polymc ];
+    environment.systemPackages = with pkgs; [ minecraft prismlauncher ];
     programs.steam.enable = true;
   };
 }
