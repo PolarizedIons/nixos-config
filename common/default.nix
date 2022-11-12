7b537066d9b93430cd5ed18fe547abc4293cb5e5
PolarizedIons <hi@polarizedions.net>
2022-11-12 18:56:17 +0200

    enable mdns

--- common/default.nix
+++ common/default.nix
@@ -87,4 +87,18 @@ in {
   networking.firewall.enable = false;
 
   nixpkgs.config.allowUnfree = true;
+
+  # mDNS
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
 }
