0eb77d957874124fadd36d89090cabeb44130ebd
Stephan <work@polarizedions.net>
2024-07-01 13:37:42 +0200

    add link to pr

--- common/system/printing.nix
+++ common/system/printing.nix
@@ -2,7 +2,7 @@
   services.printing.enable = true;
   services.printing.drivers = [
     pkgs.gutenprint
-    # Temp: broken package
+    # Temp: broken package: https://github.com/NixOS/nixpkgs/pull/321089
     # pkgs.pantum-driver 
   ];
 }
