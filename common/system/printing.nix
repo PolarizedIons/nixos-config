fc9dc705353e372a24de2a9b103925b8160ffa95
Stephan <hi@polarizedions.net>
2024-07-11 16:32:31 +0200

    re-enable pantum driver

--- common/system/printing.nix
+++ common/system/printing.nix
@@ -1,8 +1,4 @@
 { pkgs, ... }: {
   services.printing.enable = true;
-  services.printing.drivers = [
-    pkgs.gutenprint
-    # Temp: broken package: https://github.com/NixOS/nixpkgs/pull/321089
-    # pkgs.pantum-driver 
-  ];
+  services.printing.drivers = [ pkgs.gutenprint pkgs.pantum-driver ];
 }
