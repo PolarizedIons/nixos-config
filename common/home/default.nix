abf7935ee4a17b6403de014558667c17deb3d935
Stephan <me@polarizedions.net>
2024-07-28 21:12:21 +0200

    add .bin to path

--- common/home/default.nix
+++ common/home/default.nix
@@ -4,6 +4,8 @@
   programs.home-manager.enable = true;
   home.username = user.login;
   home.homeDirectory = "/home/${user.login}";
+  home.sessionPath = [ "/home/${user.login}/.bin" ];
+
   home.stateVersion = "23.11";
 
   _module.args.user = user;
