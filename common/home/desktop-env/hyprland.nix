5eb12877a4c4d2649b96d4dcf3a8be40dcb64670
Stephan <hi@polarizedions.net>
2024-07-30 21:34:04 +0200

    theme some more

--- common/home/desktop-env/hyprland.nix
+++ common/home/desktop-env/hyprland.nix
@@ -15,7 +15,39 @@ in {
         settings = {
           foreground = "#${config.colorScheme.palette.base05}";
           background = "#${config.colorScheme.palette.base00}";
-          # ...
+
+          # black
+          color0 = "#${config.colorScheme.palette.base00}";
+          color8 = "#${config.colorScheme.palette.base03}";
+
+          # red
+          color1 = "#${config.colorScheme.palette.base08}";
+          color9 = "#${config.colorScheme.palette.base08}";
+
+          # green
+          color2 = "#${config.colorScheme.palette.base0B}";
+          color10 = "#${config.colorScheme.palette.base0B}";
+
+          # yellow
+          color3 = "#${config.colorScheme.palette.base0A}";
+          color11 = "#${config.colorScheme.palette.base0A}";
+
+          # blue
+          color4 = "#${config.colorScheme.palette.base0D}";
+          color12 = "#${config.colorScheme.palette.base0D}";
+
+          # magenta
+          color5 = "#${config.colorScheme.palette.base0E}";
+          color13 = "#${config.colorScheme.palette.base0E}";
+
+          # cyan
+          color6 = "#${config.colorScheme.palette.base0C}";
+          color14 = "#${config.colorScheme.palette.base0C}";
+
+          # white
+          color7 = "#${config.colorScheme.palette.base04}";
+          color15 = "#${config.colorScheme.palette.base06}";
+
         };
       };
     };
@@ -379,7 +411,7 @@ in {
       name = "Bibata-Modern-Classic";
       size = 24;
     };
-b
+
     gtk = {
       enable = true;
 
@@ -399,6 +431,7 @@ b
       };
     };
 
+    #  Todo make this come from setup.monitors
     services.hyprpaper = let
       wallpaper = nixWallpaperFromScheme {
         scheme = config.colorScheme;
