db064da2fb1a3bf9d969986488a635cfd9ae68ad
Stephan <work@polarizedions.net>
2024-08-17 12:07:09 +0200

    downgrade to working aws vpn client

--- common/system/work.nix
+++ common/system/work.nix
@@ -4,7 +4,12 @@
   imports = [ inputs.aws-vpn-client.nixosModules.${system}.default ];
 
   config = lib.mkIf config.setup.work-mode.enable {
-    programs.awsvpnclient.enable = true;
+    programs.awsvpnclient = {
+      enable = true;
+      version = "3.13.0";
+      sha256 =
+        "e89f3bb7fc24c148e3044b807774fcfe05e7eae9e551863a38a2dcd7e0ac05f1";
+    };
 
     environment.systemPackages = with pkgs;
       lib.mkMerge [
