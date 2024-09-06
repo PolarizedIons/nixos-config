b6015da40a4817347108117560344cdb25435e1a
Stephan <hi@polarizedions.net>
2024-09-06 17:49:49 +0200

    disable because

--- common/system/vr.nix
+++ common/system/vr.nix
@@ -16,14 +16,14 @@
     # environment.systemPackages = with pkgs; [ basalt-monado ];
     # systemd.user.services.monado.environment.VIT_SYSTEM_LIBRARY_PATH = "";
 
-    boot.kernelPatches = [{
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url =
-          "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }];
+    # boot.kernelPatches = [{
+    #   name = "amdgpu-ignore-ctx-privileges";
+    #   patch = pkgs.fetchpatch {
+    #     name = "cap_sys_nice_begone.patch";
+    #     url =
+    #       "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+    #     hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+    #   };
+    # }];
   };
 }
