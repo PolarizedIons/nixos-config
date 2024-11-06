818948aa3f7533608701991f5845f3be17950faa
Stephan <hi@polarizedions.net>
2024-11-06 20:28:10 +0200

    wooting config

--- common/system/wooting.nix
+++ common/system/wooting.nix
@@ -1,32 +1,13 @@
-{ lib, config, ... }: {
+{ lib, config, pkgs, ... }: {
 
   config = lib.mkIf config.setup.wooting.enable {
-    services.udev.extraRules = ''
-      # Wooting One Legacy
+    hardware.wooting.enable = true;
 
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+    environment.systemPackages = with pkgs; [ wootility ];
 
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
-
-      # Wooting One update mode
-
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
-
-      # Wooting Two Legacy
-
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
-
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
-
-      # Wooting Two update mode
-
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
-
-      # Generic Wootings
-
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
-
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", TAG+="uaccess"
-    '';
+    users.users = builtins.listToAttrs (builtins.map (u: {
+      name = u.login;
+      value = { extraGroups = [ "input" ]; };
+    }) config.setup.users);
   };
 }
