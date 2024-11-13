fbca6e24d59455c0d087f65691ca2a653934deb7
Stephan <hi@polarizedions.net>
2024-11-13 17:45:50 +0200

    fix

--- common/home/desktop-env/hyprland.nix
+++ common/home/desktop-env/hyprland.nix
@@ -128,10 +128,10 @@ in {
             passes = 1;
           };
 
-          drop_shadow = true;
-          shadow_range = 4;
-          shadow_render_power = 3;
-          "col.shadow" = "rgba(1a1a1aee)";
+        #  drop_shadow = true;
+        #  shadow_range = 4;
+        #  shadow_render_power = 3;
+        #  "col.shadow" = "rgba(1a1a1aee)";
         };
 
         animations = { enabled = true; };
