da73eec77a370849f20aca062383c898310583cd
Stephan <hi@polarizedions.net>
2025-03-17 18:16:42 +0200

    fix

--- overlays/envision.nix
+++ overlays/envision.nix
@@ -1,5 +1,5 @@
 final: prev: {
   envision = prev.envision.overrideAttrs (oldAttrs: {
-    targetPkgs = oldAttrs.targetPkgs or [ ] ++ [ prev.pkgs.mesa.dev ];
+    targetPkgs = oldAttrs.targetPkgs or [ ] ++ [ prev.pkgs.mesa ];
   });
 }
