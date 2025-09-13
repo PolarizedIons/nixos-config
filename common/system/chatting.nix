e53c07aa5060d3d3c9b3fbf683eef7ee4fac2da8
Stephan <hi@polarizedions.net>
2025-09-13 13:14:48 +0200

    no more discord-canary

--- common/system/chatting.nix
+++ common/system/chatting.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }:
 with lib; {
   config = mkIf config.setup.chatting.enable {
-    environment.systemPackages = with pkgs; [ discord-canary noisetorch ];
+    environment.systemPackages = with pkgs; [ discord noisetorch ];
 
     # nixpkgs.overlays = [ (import ../../overlays/discord.nix) ];
   };
