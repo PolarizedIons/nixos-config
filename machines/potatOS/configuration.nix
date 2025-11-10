9fa51f8087cafb921cbdfd3f93132d78ce3ac9f2
Stephan <hi@polarizedions.net>
2025-11-10 23:41:27 +0200

    oops

--- machines/potatOS/configuration.nix
+++ machines/potatOS/configuration.nix
@@ -31,6 +31,9 @@
   setup.chatting.enable = true;
   setup.music.enable = true;
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
