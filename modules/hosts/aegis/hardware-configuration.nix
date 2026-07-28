393dd02c6becfe8b94d3522f853c12f1d45bfde0
Stephan <hi@polarizedions.net>
2026-07-28 18:04:01 +0200

    fix

--- modules/hosts/aegis/hardware-configuration.nix
+++ modules/hosts/aegis/hardware-configuration.nix
@@ -23,9 +23,7 @@
         "nvme"
         "xhci_pci"
         "ahci"
-        "usb_storage"
         "usbhid"
-        "sd_mod"
       ];
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-amd" ];
@@ -39,18 +37,15 @@
       fileSystems."/boot" = {
         device = "/dev/disk/by-uuid/D3FA-7448";
         fsType = "vfat";
+        options = [
+          "fmask=0022"
+          "dmask=0022"
+        ];
       };
 
       swapDevices = [
         { device = "/dev/disk/by-uuid/11bca29d-a96e-4a8f-8031-4e91305db416"; }
       ];
-
-      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-      # (the default) this is the recommended approach. When using systemd-networkd it's
-      # still possible to use this option, but it's recommended to use it in conjunction
-      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-      networking.useDHCP = lib.mkDefault true;
-      # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
