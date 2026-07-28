56269fcc4356c258e365e8c0020be8c9077d65f1
Stephan <hi@polarizedions.net>
2026-07-28 19:26:49 +0200

    update flake.lock

--- modules/features/wooting/default.nix
+++ modules/features/wooting/default.nix
@@ -1,7 +1,7 @@
-{ ... }:
+{ config, ... }:
 
 {
-  flake.nixosModule.wooting = { pkgs, config, ... }: {
+  flake.nixosModule.wooting = { pkgs, ... }: {
     hardware.wooting.enable = true;
     environment.systemPackages = with pkgs; [ wootility ];
     users.users."${config.username}".extraGroups = [ "input" ];
