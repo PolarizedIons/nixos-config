d7e0858572e6b2cafeee2e7a3e3fbf31127fe465
Stephan <me@polarizedions.net>
2026-08-22 20:45:25 +0200

    obs

--- modules/features/obs/default.nix
+++ modules/features/obs/default.nix
@@ -6,32 +6,18 @@
     programs.obs-studio = {
       enable = true;
       enableVirtualCamera = true;
-      package = self.packages."${pkgs.stdenv.hostPlatform.system}".obs;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+        obs-vaapi # AMD hardware acceleration
+        obs-gstreamer
+        obs-vkcapture
+        obs-move-transition
+        obs-3d-effect
+      ];
     };
 
     security.polkit.enable = true;
-  };
-
-  perSystem = { pkgs, ... }: {
-    packages.obs =
-      (pkgs.wrapOBS {
-        plugins = with pkgs.obs-studio-plugins; [
-          wlrobs
-          obs-backgroundremoval
-          obs-pipewire-audio-capture
-          obs-vaapi # AMD hardware acceleration
-          obs-gstreamer
-          obs-vkcapture
-          obs-move-transition
-          obs-3d-effect
-        ];
-      })
-      # https://github.com/NixOS/nixpkgs/issues/555398
-      .overrideAttrs
-        (oldAttrs: {
-          buildCommand =
-            builtins.replaceStrings [ "rm -r $out/share/obs/obs-plugins" "echo" ] [ "" "# echo" ]
-              oldAttrs.buildCommand;
-        });
   };
 }
